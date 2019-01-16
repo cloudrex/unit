9c75798bd0c66a9de874d04e67b79b9267b40afd
CloudRex <cloudrex@outlook.com>
2019-01-16 01:23:41 -0500

    Fix division by zero

--- src/util.ts
+++ src/util.ts
@@ -7,8 +7,16 @@ export default abstract class Util {
     public static isEmpty(input: any): boolean {
         return input === undefined || input === null || input.toString().trim() === "" || (Array.isArray(input) && input.length === 0);
     }
-    
+
     public static percentage(amount: number, max: number): number {
+        // Prevent NaN from division by zero
+        if (amount === 0 && max === 0) {
+            return 100;
+        }
+        else if (max === 0) {
+            return 0;
+        }
+
         return Math.round(amount / max) * 100;
     }
 }
