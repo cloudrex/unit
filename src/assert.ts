3da38a87fe6e1335e27795606ad67b9eeb4ef666
CloudRex <cloudrex@outlook.com>
2019-01-18 22:33:39 -0500

    Make assert methods chainable

--- src/assert.ts
+++ src/assert.ts
@@ -2,7 +2,7 @@ import colors from "colors";
 import {Constraint, Action} from "./runner";
 
 export default abstract class Assert {
-    public static that(input: any, ...constraints: Constraint[]): void {
+    public static that(input: any, ...constraints: Constraint[]): Assert {
         for (const constraint of constraints) {
             const result: string | null = constraint(input);
 
@@ -10,27 +10,35 @@ export default abstract class Assert {
                 Assert.complain(result);
             }
         }
+
+        return this;
     }
 
-    public static equal(entity1: any, entity2: any): void {
+    public static equal(entity1: any, entity2: any): Assert {
         if (entity1 !== entity2) {
             Assert.complain(`Expected '${entity1}' to equal '${entity2}'`);
         }
+
+        return this;
     }
 
-    public static notEqual(entity1: any, entity2: any): void {
+    public static notEqual(entity1: any, entity2: any): Assert {
         if (entity1 === entity2) {
             Assert.complain(`Expected '${entity1}' to not equal '${entity2}'`);
         }
+
+        return this;
     }
 
-    public static true(input: any): void {
+    public static true(input: any): Assert {
         if (input !== true) {
             Assert.complain(`Expected '${input}' to be true`);
         }
+
+        return this;
     }
 
-    public static throws(method: Action, message?: string): void {
+    public static throws(method: Action, message?: string): Assert {
         let resultError: Error | null = null;
         let methodName: string = method.name;
 
@@ -52,12 +60,16 @@ export default abstract class Assert {
         else if (message !== undefined && resultError.message !== message) {
             Assert.complain(`Expected '${methodName}' to throw with message '${message}' but got '${resultError.message}' instead`);
         }
+
+        return this;
     }
 
-    public static false(input: any): void {
+    public static false(input: any): Assert {
         if (input !== false) {
             Assert.complain(`Expected '${input}' to be false`);
         }
+
+        return this;
     }
 
     protected static complain(message: string): void {
